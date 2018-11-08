53696888ac3edfb27e86a44082da7c43b98e9330
Clarice <claricostello@outlook.com>
2018-11-08 14:16:51 -0800

    added prototype to store cart data in local storage

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -13,6 +13,8 @@ Cart.prototype.addItem = function(product, quantity) {
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  var cartData = JSON.stringify(this.items);
+  localStorage.setItem('cartData', cartData);
 };
 
 Cart.prototype.removeItem = function(item) {
