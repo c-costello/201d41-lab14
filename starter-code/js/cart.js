7e3c7d49a78d5273f9d681968cdf890bc7bed59b
Clarice <claricostello@outlook.com>
2018-11-09 08:23:57 -0800

    created table rows based on cart interations

--- starter-code/js/cart.js
+++ starter-code/js/cart.js
@@ -30,10 +30,15 @@ function clearCart() {
   
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
-  
-  // TODO: Find the table body
 
+  // TODO: Find the table body
+  var tbodyEl = document.getElementsByTagName('tbody'); 
   // TODO: Iterate over the items in the cart
+  for (var i = 0; i < cart.length; i++){
+    var trEl = document.createElement('tr');
+    tbodyEl.appendChild(trEl);
+    
+  }
   // TODO: Create a TR
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
